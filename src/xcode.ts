0255b4cf03e0641f45d4e59ecd3912cb60a399bd
Jozef Izso <izso@cisco.com>
2024-10-29 13:11:37 +0100

    Improve error reporting when determining Xcode version

--- src/xcode.ts
+++ src/xcode.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
 export async function getXcodeVersion(): Promise<number> {
@@ -7,12 +8,18 @@ export async function getXcodeVersion(): Promise<number> {
       stdout: (data: Buffer) => {
         output += data.toString()
       }
-    }
+    },
+    silent: !core.isDebug()
   }
   await exec.exec('xcodebuild', ['-version'], options)
   const match = output.match(/Xcode (\d+)/)
   if (match) {
     return parseInt(match[1], 10)
+  } else {
+    core.warning(
+      `Failed to determine Xcode version from command 'xcodebuild -version'.`
+    )
+    core.info(output)
+    return 0
   }
-  throw new Error('Unable to determine Xcode version')
 }
